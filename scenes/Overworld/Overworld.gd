9047598681dee394880ebb9f71dff44dd78544f5
GreenCastleB <secondepoch@gmail.com>
2024-07-13 21:10:15 -0500

    Updating Overworld Singleton
    Stores & recalls camera Y position when overworld is loaded & unloaded.

--- scenes/Overworld/Overworld.gd
+++ scenes/Overworld/Overworld.gd
@@ -7,9 +7,19 @@ extends Control
 @onready var MapOriginY = BGArt.texture.get_height() - 100;
 @onready var MapNodeSCENE = preload("res://scenes/Overworld/MapNode.tscn");
 
+# variables which are loaded to / recalled from the singleton
+var Camera_CurrentY = null;
+
+func _enter_tree():
+	OverworldSingleton.loadStuff(self);
+
 func _ready():
 	# line up camera with center of map artwork
 	CameraObj.position.x = -(size.x - BGArt.texture.get_width())/2;
+	
+	# if there was a stored camera Y, return there
+	if(Camera_CurrentY != null):
+		CameraObj.position.y = Camera_CurrentY;
 	
 	# spawn a map node
 	var newMapNode = MapNodeSCENE.instantiate();
@@ -20,3 +30,7 @@ func _process(_delta):
 	# scroll camera down, stop at bottom of map artwork
 	if(CameraObj.position.y < BGArt.texture.get_height() - size.y):
 		CameraObj.position.y += 4;
+		Camera_CurrentY = CameraObj.position.y;
+
+func _exit_tree():
+	OverworldSingleton.saveStuff(self);
--- scenes/Overworld/OverworldManager.gd
+++ scenes/Overworld/OverworldManager.gd
@@ -1,5 +1,17 @@
 class_name OverworldManager
 extends Node
 
+# variables which need to be stored when overworld scene is unloaded
+var Camera_CurrentY = null;
+
 func _ready():
 	print("OverworldManager loaded");
+
+func loadStuff(CurrentWorld):
+	# overworld scene is asking for data
+	if(Camera_CurrentY != null):
+		CurrentWorld.Camera_CurrentY = Camera_CurrentY;
+
+func saveStuff(CurrentWorld):
+	# overworld scene is being unloaded & giving data
+	Camera_CurrentY = CurrentWorld.Camera_CurrentY;

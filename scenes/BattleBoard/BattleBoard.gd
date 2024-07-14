6c8c9490efd9a4c2c38f1a7fcd299e6de3fa2c23
dnevill <dnevill@gmail.com>
2024-07-14 14:26:17 -0600

    Pegs, 20 hole, gutter basic functionality
    Also added a small amount of randomness to the current placeholder player shot until we get the 'real' shot taking scene.

--- scenes/BattleBoard/BattleBoard.gd
+++ scenes/BattleBoard/BattleBoard.gd
@@ -1,16 +1,20 @@
 extends Node2D
 var disc_template = preload("res://scenes/Discs/Player Disc Template/PlayerDiscTemplate.tscn")
+var peg = preload("res://scenes/Board Obstacles/Peg/Peg.tscn")
 
 var current_disc : Disc = null
+const PEG_COUNT = 8
 
+@onready var peg_radius = $Sprite15PT.get_rect().size.x / 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	place_pegs(peg_radius, PEG_COUNT)
 
 func _input(event):
-	if Input.is_action_just_released("ui_up"):
-		current_disc.apply_central_impulse(($Sprite20PT.position - current_disc.position).normalized() * 600)
+	if Input.is_action_just_released("ui_up") and current_disc != null:
+		current_disc.apply_central_impulse(($Sprite20PT.position - current_disc.position).normalized().rotated(randf_range(-0.05, 0.05)) * 600)
+		current_disc = null
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			var enemydisc = disc_template.instantiate()
 			enemydisc.position = get_global_transform_with_canvas().affine_inverse() * event.position
@@ -23,5 +27,18 @@ func _on_edge_area_2d_input_event(viewport, event, shape_idx):
 			current_disc.position = get_global_transform_with_canvas().affine_inverse() * event.position
 			add_child(current_disc)
 
+func place_pegs(radius, peg_count):
+	var step_size = TAU / peg_count
+	for angle in Vector3(0.0, TAU, step_size):
+		var peg_to_place = peg.instantiate()
+		peg_to_place.position = Vector2.RIGHT.rotated(angle + step_size / 2) * radius
+		add_child(peg_to_place)
 
 
+
+func _on_gutter_area_body_entered(body):
+	if body is Disc:
+		body.set_collision_mask_value(2, true)
+		body.set_collision_mask_value(1, false)
+		body.set_collision_layer_value(1, false)
+		body.set_collision_layer_value(2, true)
--- scenes/Discs/Player Disc Template/PlayerDisc.gd
+++ scenes/Discs/Player Disc Template/PlayerDisc.gd
@@ -11,6 +11,13 @@ extends RigidBody2D
 var previous_velocity = 0
 var touched = false
 
+var freeing_up = false
+
+var inhole_south = false
+var inhole_north = false
+var inhole_west = false
+var inhole_east = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	mass = pmass
@@ -28,9 +35,6 @@ func _on_body_entered(body):
 	print(body)
 	if not touched:
 		print("We not touched yet")
-		if typeof(body) == typeof(Disc):
-			var dbody : Disc = body
-			dbody.touched = true
 		var impulse = Vector2(linear_velocity - previous_velocity).length_squared()
 		const min_pitch = 1
 		const max_pitch = 1.4
@@ -41,6 +45,67 @@ func _on_body_entered(body):
 		var new_pitch = clampf(remap(impulse,min_impulse, max_impulse, min_pitch, max_pitch),min_pitch, max_pitch)
 		var new_volume = clampf(remap(impulse,min_impulse, max_impulse, min_dB, max_dB),min_dB, max_dB)
 		print(str(impulse) + " vol: " + str(new_volume) + " pitch: " + str(new_pitch))
-		$AudioStreamPlayer2D.pitch_scale = new_pitch
-		$AudioStreamPlayer2D.volume_db = new_volume
-		$AudioStreamPlayer2D.play()
+		if body is Peg:
+			print(body.name)
+			$A2DPegHit.pitch_scale = new_pitch
+			$A2DPegHit.volume_db = new_volume
+			$A2DPegHit.play()
+		else:
+			print(body.name)
+			$A2DChockHit.pitch_scale = new_pitch
+			$A2DChockHit.volume_db = new_volume
+			$A2DChockHit.play()
+			if body is Disc:
+				var dbody : Disc = body
+				dbody.touched = true
+
+
+func check_pop():
+	if not freeing_up:
+		var hole : int = int(inhole_north) + int(inhole_south) + int(inhole_east) + int(inhole_west)
+		print(hole)
+		if hole > 2:
+			print("Playing pop noise from " + str(self))
+			freeing_up = true
+			$A2DHolePop.play()
+
+func _on_overlap_check_s_area_entered(area):
+	if area is Hole20Area:
+		inhole_south = true
+		check_pop()
+
+func _on_overlap_check_s_area_exited(area):
+	if area is Hole20Area:
+		inhole_south = false
+
+func _on_overlap_check_n_area_entered(area):
+	if area is Hole20Area:
+		inhole_north = true
+		check_pop()
+
+func _on_overlap_check_n_area_exited(area):
+	if area is Hole20Area:
+		inhole_north = false
+		
+func _on_overlap_check_w_area_entered(area):
+	if area is Hole20Area:
+		inhole_west = true
+		check_pop()
+
+func _on_overlap_check_w_area_exited(area):
+	if area is Hole20Area:
+		inhole_west = false
+		
+func _on_overlap_check_e_area_entered(area):
+	if area is Hole20Area:
+		inhole_east = true
+		check_pop()
+
+func _on_overlap_check_e_area_exited(area):
+	if area is Hole20Area:
+		inhole_east = false
+
+
+func _on_a_2d_hole_pop_finished():
+	print("Freeing this one " + str(self))
+	queue_free()

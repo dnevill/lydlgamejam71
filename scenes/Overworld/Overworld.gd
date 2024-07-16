530baf49ff88f3e2700deb217b390ee73ab19360
GreenCastleB <secondepoch@gmail.com>
2024-07-16 15:34:41 -0500

    OW Camera improvements
    Also placeholder HUD frame.

--- scenes/Overworld/Overworld.gd
+++ scenes/Overworld/Overworld.gd
@@ -6,14 +6,16 @@ extends Control
 # all changes have to go through the Singleton
 
 const MapNodeSIZE = 64;
-const MapNodePADDING = 50;
+const MapNodePADDING = 50; # pixels between Map Nodes
+const MapOriginPADDING = 150; # pixels from bottom Map begins
+const CAMERA_SPEED = 4; # pixels per frame
 
 @onready var CameraObj = $CameraObj;
 @onready var MapNodes = $WorldNode/MapNodes;
 @onready var BGArt = $WorldNode/BGLayer/BGArtwork;
 @onready var MidpointX = size.x / 2;
 @onready var MapOriginX = BGArt.texture.get_width() / 2;
-@onready var MapOriginY = BGArt.texture.get_height() - MapNodePADDING;
+@onready var MapOriginY = BGArt.texture.get_height() - MapOriginPADDING;
 @onready var MapNodeSCENE = preload("res://scenes/Overworld/MapNode.tscn");
 
 # variables which are loaded to / recalled from the singleton
@@ -21,6 +23,8 @@ var Camera_CurrentY = null;
 
 # variables that can be managed by the scene
 var PlayerCurrNode:OverworldNode = null;
+var verticalMapProgress:int = 0;
+@onready var desiredCameraY:float = BGArt.texture.get_height() - size.y - verticalMapProgress;
 
 func _enter_tree():
 	OverworldSingleton.loadStuff(self);
@@ -28,20 +32,23 @@ func _enter_tree():
 func _drawMapNodes(drawingNode:OverworldNode, drawLocX:float, drawLocY:float):
 	print("Overworld:: _drawMapNodes type" + str(drawingNode.nodeType) + " @ " + str(drawLocX) + ", " + str(drawLocY));
 	
-	######################################################################
+	###########################
+	## CREATE THIS MAP NODE  ##
+	###########################
 	
 	# draw the Overworld map node
 	var newMapNode = MapNodeSCENE.instantiate();
 	newMapNode.get_child(1).set_frame(drawingNode.nodeType); # main sprite
 	newMapNode.get_child(0).set_frame(randi() % 4); # lilypad
-	newMapNode.get_child(0).play();
 	newMapNode.position = Vector2(drawLocX, drawLocY);
 	MapNodes.add_child(newMapNode);
 	
-	# link back to structure
+	# link Node2D back to structure
 	drawingNode.localSceneLink = newMapNode;
 	
-	######################################################################
+	###########################
+	## RECURSIVELY DRAW KIDS ##
+	###########################
 	
 	var numberOfChildren = drawingNode.childNodes.size();
 	if(numberOfChildren == 1):
@@ -83,6 +90,10 @@ func _activateNextNodes():
 		var thisChildNode:OverworldNode = PlayerCurrNode.childNodes[thisChildIdx];
 		thisChildNode.localSceneLink.get_child(0).set("modulate",Color("00ff00"));
 
+func _cameraIsAtDesiredLoc():
+	# enable inputs, etc.
+	_activateNextNodes();
+
 func _ready():
 	# line up camera with center of map artwork
 	CameraObj.position.x = -(size.x - BGArt.texture.get_width())/2;
@@ -93,13 +104,43 @@ func _ready():
 	
 	_drawMapNodes(OverworldSingleton.mapGetRoot(), MapOriginX, MapOriginY);
 	_findPlayerCurrNode();
-	_activateNextNodes();
+	_updateVerticalMapProgress();
+	
+	if(Camera_CurrentY == desiredCameraY):
+		_cameraIsAtDesiredLoc();
+
+func _updateVerticalMapProgress():
+	verticalMapProgress = OverworldSingleton.mapGetRoot().localSceneLink.position.y - OverworldSingleton.mapGetRoot().findFurthestLaunched().localSceneLink.position.y;
+	desiredCameraY = BGArt.texture.get_height() - size.y - verticalMapProgress;
+
+func _adjustCamera():
+	if(abs(int(CameraObj.position.y) - desiredCameraY) > CAMERA_SPEED):
+		# further than CAMERA_SPEED away
+		if(CameraObj.position.y < desiredCameraY):
+			# camera must go down
+			CameraObj.position.y += CAMERA_SPEED;
+		else:
+			# camera must go up
+			CameraObj.position.y -= CAMERA_SPEED;
+	elif(abs(int(CameraObj.position.y) - desiredCameraY) > 1):
+		# very close
+		if(CameraObj.position.y < desiredCameraY):
+			# camera must go down
+			CameraObj.position.y += 1;
+		else:
+			# camera must go up
+			CameraObj.position.y -= 1;
+	else:
+		# camera is at desired location.
+		if(Camera_CurrentY != CameraObj.position.y):
+			print("Overworld:: _adjustCamera just arrived at "+str(CameraObj.position.y));
+			
+			# update camera location for singleton
+			Camera_CurrentY = CameraObj.position.y;
+			_cameraIsAtDesiredLoc();
 
 func _process(_delta):
-	# scroll camera down, stop at bottom of map artwork
-	if(CameraObj.position.y < BGArt.texture.get_height() - size.y):
-		CameraObj.position.y += 4;
-		Camera_CurrentY = CameraObj.position.y;
+	_adjustCamera();
 
 func _exit_tree():
 	OverworldSingleton.saveStuff(self);

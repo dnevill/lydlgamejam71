0c1973060999e70e59592b47a9646f856b7b2788
dnevill <dnevill@gmail.com>
2024-07-21 13:09:39 -0600

    Fix options we don't use, banner, main menu music

--- build and butler push.bat
+++ build and butler push.bat
@@ -1,5 +1,5 @@
 E:\godotwithoutnet\Godot_v4.2.2-stable_win64_console.exe --headless --export-release Web ./build/web/index.html
-butler push ./build/web dnevill/lydlbujam71:html5
+butler push ./build/web dnevill/croakanole:html5
 timeout 10
-butler status dnevill/lydlbujam71:html5
+butler status dnevill/croakanole:html5
 @pause
